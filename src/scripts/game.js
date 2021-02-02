9d0c2629fcd5193382c9f8700ebc8f125a0d80f1
Filipe Moreira <filimor@posteo.com.br>
2021-02-02 18:01:17 -0300

    Perform a general review in the code

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,36 +1,36 @@
+const FIELD_BACKGROUNG = '#fafafa';
 const FIELD_HEIGHT = 150;
 const FIELD_WIDTH = 600;
-const PLAYER_HEIGHT = 46;
-const PLAYER_WIDTH = 44;
-const PLAYER_START_X = 22;
-const PLAYER_START_Y = 127;
 const FPS = 1 / 20;
+const PLAYER_POS = {
+  'NORMAL_WIDTH': 44,
+  'NORMAL_HEIGHT': 46,
+  'NORMAL_X': 22,
+  'NORMAL_Y': 127,
+  'DUCK_WIDTH': 58,
+  'DUCK_HEIGHT': 30,
+  'DUCK_X': 29,
+  'DUCK_Y': 135,
+};
 const KEYBINDS = {
   ' ': 'jump',
   'ArrowDown': 'duck'
 };
 
-// TODO: Couldn't this be inside a class?
-function randomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-// TODO: use ES6 prop names
 class Entity {
   constructor(position, speed, direction) {
     this.position = position;
     this.speed = speed;
     this._direction = direction;
-    this.timer = 0;
     this.width = 5;
     this.height = 5;
     this.hp = 1;
-    // TODO: reduce collision rect size
+    this.padding = -8;
     this.collisionRect = new Rectangle(
-      this.position.x - this.width / 2,
-      this.position.y - this.height / 2,
-      this.width,
-      this.height
+      this.position.x - this.width / 2 + this.padding,
+      this.position.y - this.height / 2 + this.padding,
+      this.width + this.padding,
+      this.height + this.padding
     );
   }
 
@@ -43,29 +43,25 @@ class Entity {
   }
 
   getCollisionRect() {
-    this.collisionRect.x = this.position.x - this.width / 2;
-    this.collisionRect.y = this.position.y - this.height / 2;
-    this.collisionRect.width = this.width;
-    this.collisionRect.height = this.height;
+    this.collisionRect.x = this.position.x - this.width / 2 + this.padding;
+    this.collisionRect.y = this.position.y - this.height / 2 + this.padding;
+    this.collisionRect.width = this.width + this.padding;
+    this.collisionRect.height = this.height + this.padding;
 
     return this.collisionRect;
   }
 
   update(fps) {
-    this.timer += fps;
   }
 }
-
-// TODO: create subclasse of enemy with different sizes
 class Enemy extends Entity {
-  constructor(position, speed) {
+  constructor(position, speed, rank) {
     super(position, speed, new Vector2d(-1, 0));
 
-    // TODO: Implement different enemy sizes (sub-classing?)
+    // TODO: Implement different enemy sizes
     this.width = 34;
     this.height = 35;
-    // TODO: Do I need this field?
-    this.timer = 0;
+    this.rank = rank;
   }
 
   update(fps) {
@@ -80,24 +76,22 @@ class Enemy extends Entity {
 class Player extends Entity {
   constructor(position) {
     super(position, 0, new Vector2d(0, 0));
-    this.width = PLAYER_WIDTH;
-    this.height = PLAYER_HEIGHT;
-    this.gravity = 9.8 * 6;
-    this.impulse = 50;
+    this.width = PLAYER_POS.NORMAL_WIDTH;
+    this.height = PLAYER_POS.NORMAL_HEIGHT;
     this.ducking = false;
     this.jumping = false;
-    this.speed = 100;
+    this.speed = 160;
+    this.status = 0;
   }
 
   jump() {
-    if (this.jumping) {
+    if (this.jumping || this.ducking) {
       return;
     }
 
     this.jumping = true;
 
-    // TODO: Fix the FPS used
-    let maxJump = PLAYER_HEIGHT / 2;
+    let maxJump = PLAYER_POS.NORMAL_HEIGHT / 2;
     let upInterval = setInterval(() => {
       if (this.position.y <= maxJump) {
         this.position.y = maxJump;
@@ -105,8 +99,8 @@ class Player extends Entity {
 
         let downInterval = setInterval(() => {
           this.direction.set(0, 1);
-          if (this.position.y >= PLAYER_START_Y) {
-            this.position.y = PLAYER_START_Y;
+          if (this.position.y >= PLAYER_POS.NORMAL_Y) {
+            this.position.y = PLAYER_POS.NORMAL_Y;
             this.direction.set(0, 0);
             clearInterval(downInterval);
             this.jumping = false;
@@ -119,8 +113,25 @@ class Player extends Entity {
   }
 
   duck(enable) {
+    if (this.jumping) {
+      return;
+    }
+
     this.ducking = enable;
-    console.log('duck to be implemented');
+
+    if (enable) {
+      this.width = PLAYER_POS.DUCK_WIDTH;
+      this.height = PLAYER_POS.DUCK_HEIGHT;
+      this.position.x = PLAYER_POS.DUCK_X;
+      this.position.y = PLAYER_POS.DUCK_Y;
+      this.status = 1;
+    } else {
+      this.width = PLAYER_POS.NORMAL_WIDTH;
+      this.height = PLAYER_POS.NORMAL_HEIGHT;
+      this.position.x = PLAYER_POS.NORMAL_X;
+      this.position.y = PLAYER_POS.NORMAL_Y;
+      this.status = 0;
+    }
   }
 
   update(fps) {
@@ -137,13 +148,13 @@ class PlayerActions {
         if (!game.started || game.gameOver) {
           game.start();
         } else {
-          if (game.getPlayer()) {
+          if (game.player) {
             game.player.jump();
           }
         }
       },
       'duck': () => {
-        if (game.getPlayer()) {
+        if (game.player) {
           game.player.duck(true);
         }
       }
@@ -151,8 +162,8 @@ class PlayerActions {
 
     this.endActions = {
       'duck': () => {
-        if (game.getPlayer()) {
-          game.getPlayer.duck(false);
+        if (game.player) {
+          game.player.duck(false);
         }
       }
     };
@@ -163,7 +174,6 @@ class PlayerActions {
       return;
     }
 
-    // TODO: WTF is that?
     let f;
     if (f = this.startActions[playerAction]) {
       f();
@@ -173,13 +183,10 @@ class PlayerActions {
   }
 
   endAction(id) {
-    // TODO: remove duplicated code
-
     let index = this.ongoingActions.findIndex(x => x.identifier === id);
 
     let f;
     if (index >= 0) {
-      // TODO: WTF again?
       if (f = this.endActions[this.ongoingActions[index].playerAction]) {
         f();
       }
@@ -215,7 +222,6 @@ class Sprite {
         0, 0, this.spriteImage.width, this.spriteImage.height
       );
 
-      // TODO: It's really necessary?
       const data = sourceData.data;
       for (let i = 0; i < data.length; i += 4) {
         data[i] = red;
@@ -244,16 +250,24 @@ class Renderer {
     this.canvas = document.querySelector('#game-layer');
     this.context = this.canvas.getContext('2d');
 
-    this.playerSprite = new Sprite('/src/images/dino.png', 4, 8, 0, 0, 0);
+    this.playerSprites = [
+      new Sprite('/src/images/dino.png', 4, 8, 0, 0, 0),
+      new Sprite('/src/images/dino_down.png', 2, 4, 0, 0, 0),
+    ];
     this.enemySprites = [
       new Sprite('/src/images/small_cactus1.png', 1, 2, 0, 0, 0),
-      new Sprite('/src/images/small_cactus2.png', 1, 1, 0, 0, 0)
+      new Sprite('/src/images/small_cactus2.png', 1, 2, 0, 0, 0),
+      new Sprite('/src/images/small_cactus3.png', 1, 2, 0, 0, 0),
+      new Sprite('/src/images/big_cactus1.png', 1, 2, 0, 0, 0),
+      new Sprite('/src/images/big_cactus2.png', 1, 2, 0, 0, 0),
+      new Sprite('/src/images/big_cactus3.png', 1, 2, 0, 0, 0),
+      new Sprite('/src/images/big_cactus4.png', 1, 2, 0, 0, 0),
+      new Sprite('/src/images/bird.png', 2, 4, 0, 0, 0),
     ];
 
-    this.sprites = [].concat(this.playerSprite, this.enemySprites);
+    this.sprites = [].concat(this.playerSprites, this.enemySprites);
   }
 
-  // TODO: convert entity.position.x = entity.width / 2 to a prop?
   drawSprite(sprite, entity) {
     const frame = sprite.image.width / sprite.frames;
     this.context.drawImage(
@@ -274,14 +288,14 @@ class Renderer {
       sprite.update(fps);
     }
 
-    this.context.fillStyle = '#fafafa';
+    this.context.fillStyle = FIELD_BACKGROUNG;
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-    for (const entity of game.getEntities()) {
+    for (const entity of game.entities) {
       if (entity instanceof Enemy) {
-        this.drawSprite(this.enemySprites[0], entity);
+        this.drawSprite(this.enemySprites[entity.rank], entity);
       } else if (entity instanceof Player) {
-        this.drawSprite(this.playerSprite, entity);
+        this.drawSprite(this.playerSprites[entity.status], entity);
       }
     }
 
@@ -293,7 +307,7 @@ class Renderer {
     const highScoresElement = document.querySelector('#high-scores');
     const titleElement = document.querySelector('#title');
 
-    const scoreText = 'Score' + Math.round(game.score);
+    const scoreText = + Math.round(game.score);
     if (scoreElement.innerHTML != scoreText) {
       scoreElement.innerHTML = scoreText;
     }
@@ -304,15 +318,10 @@ class Renderer {
       for (let i = 0; i < scores.length; i++) {
         document.querySelector('#score' + i).innerHTML = scores[i];
       }
+
       highScoresElement.style.display = 'block';
       titleElement.style.display = 'none';
-      console.log(highScoresElement);
     } else {
-      highScoresElement.style.display = 'none';
-      titleElement.style.display = 'none';
-    }
-
-    if (game.started) {
       highScoresElement.style.display = 'none';
       titleElement.style.display = 'none';
     }
@@ -331,23 +340,18 @@ class Physics {
   }
 
   update(fps) {
-    for (const entity of game.getEntities()) {
+    for (const entity of game.entities) {
       this.velocityStep.set(entity.direction.x, entity.direction.y);
       this.velocityStep.scalarMultiply(entity.speed * fps);
       entity.position.add(this.velocityStep);
     }
 
-    for (const enemy of game.getEnemies()) {
-      this.collide(game.getPlayer(), enemy);
-    }
-
-    if (game.getPlayer() && game.getPlayer().jumping) {
-
+    for (const enemy of game.enemies) {
+      this.collide(game.player, enemy);
     }
   }
 }
 
-// TODO: remove unused methods
 class Vector2d {
   constructor(x, y) {
     this.x = x;
@@ -359,32 +363,14 @@ class Vector2d {
     this.y = y;
   }
 
-  clone() {
-    return new Vector2d(this.x, this.y);
-  }
-
   add(vector2) {
     this.x += vector2.x;
     this.y += vector2.y;
   }
 
-  subtract(vector2) {
-    this.x -= vector2.x;
-    this.y -= vector2.y;
-  }
-
-  static vectorLength(vector) {
-    return Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
-  }
-
   scalarMultiply(scalarValue) {
     this.x *= scalarValue;
     this.y *= scalarValue;
-  }
-
-  static vectorNormalize(vector) {
-    const reciprocal = 1.0 / (this.vectorLength(vector) + Number.EPSILON);
-    return this.scalarMultiply(reciprocal);
   }
 }
 
@@ -401,10 +387,6 @@ class Rectangle {
     this.y = y;
     this.width = width;
     this.height = height;
-  }
-
-  static clone() {
-    return new Rectangle(this.x, this.y, this.width, this.height);
   }
 
   left() {
@@ -429,36 +411,9 @@ class Rectangle {
       this.top() <= rectangle2.bottom() &&
       this.bottom() >= rectangle2.top();
   }
-
-  containsPoint(x, y) {
-    return this.left() <= x &&
-      x <= this.right() &&
-      this.top() <= y &&
-      y <= this.bottom();
-  }
-
-  //TODO: It's really necessary?f
-  static union(rectangle2) {
-    if (rectangle2 === undefined) {
-      return;
-    }
-
-    const x = Math.min(this.x, rectangle2.x);
-    const y = Math.min(this.y, rectangle2.y);
-    const width = Math.max(this.right(), rectangle2.right() -
-      Math.min(this.left(), rectangle2.left()));
-    const height = Math.max(this.bottom, rectangle2.bottom() -
-      Math.min(this.top(), rectangle2.top()));
-
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
 }
 
 class Keyboard {
-  // TODO: Detach keyboard from playerActions
   keyDown(event) {
     const key = event.key;
 
@@ -478,47 +433,78 @@ class Keyboard {
   }
 }
 
-class Game {
-  constructor() {
-    this.fieldRect = new Rectangle(0, 0, FIELD_WIDTH, FIELD_HEIGHT);
-    this.enemySpeed = 150;
-    this.spawnInterval = 10;
-    this.entities = [];
-    this.enemies = [];
-    this.player = undefined;
-    this.started = false;
-    this.gameOver = false;
-    this.score = 0;
-    this.lastFps = 0;
-    // TODO: Move the high scores to another class
-    this.highScores = [];
-    this.updateFunc = () => this.update();
-
+class io {
+  static getHighScores() {
     if (typeof(Storage) !== undefined) {
       try {
-        this.highScores = JSON.parse(localStorage.dinoScores);
+        return JSON.parse(localStorage.dinoScores);
       } catch (e) {
-        this.highScores = [];
+        return [];
       }
     }
   }
 
+  static SaveHighScores(scores) {
+    if (typeof(Storage) !== undefined) {
+      localStorage.dinoScores = JSON.stringify(scores);
+    }
+  }
+}
+
+class Game {
+  constructor() {
+    this._fieldRect = new Rectangle(0, 0, FIELD_WIDTH, FIELD_HEIGHT);
+    this.enemySpeed = 150;
+    this.SpawnRate =  20;
+    this._entities = [];
+    this._enemies = [];
+    this._player = undefined;
+    this.started = false;
+    this.gameOver = false;
+    this.score = 0;
+    this.highScores = io.getHighScores();
+    this.updateFunction = () => this.update();
+  }
+
+  get entities() {
+    return this._entities;
+  }
+
+  get player() {
+    return this._player;
+  }
+
+  get enemies() {
+    return this._enemies;
+  }
+
+  get fieldRect() {
+    return this._fieldRect;
+  }
+
   start() {
-    this.addEntity(new Player(new Vector2d(PLAYER_START_X, PLAYER_START_Y)));
+    if (this.player === undefined) {
+      this.addEntity(new Player(new Vector2d(PLAYER_POS.NORMAL_X, PLAYER_POS.NORMAL_Y)));
+    }
 
     if (!this.started) {
+      if (this.gameOver) {
+        this.removeEntities(this._enemies);
+        this.gameOver = false;
+      }
+
       window.requestAnimationFrame(() => this.update());
       this.started = true;
     }
   }
 
   addEntity(entity) {
-    this.entities.push(entity);
+    this._entities.push(entity);
 
     if (entity instanceof Player) {
-      this.player = entity;
+      this._player = entity;
     } else if (entity instanceof Enemy) {
-      this.enemies.push(entity);
+      this._enemies.push(entity);
     }
   }
 
@@ -527,80 +513,62 @@ class Game {
       return;
     }
 
-    this.entities = this.entities.filter(x => !entities.includes(x));
-    this.enemies = this.enemies.filter(x => !entities.includes(x));
+    this._entities = this._entities.filter(x => !entities.includes(x));
+    this._enemies = this._enemies.filter(x => !entities.includes(x));
 
-    if (entities.includes(this.player)) {
-      this.player = undefined;
+    if (entities.includes(this._player)) {
+      this._player = undefined;
     }
   }
 
-  update(newFps) {
-    // TODO: remove this and fix FPS calculation
-    const fps = FPS;
-
-    // TODO: update the score here?
-    //score++;
-
-    //const fps = Math.min((newFps - this.lastFps) / 1000, FPS);
-    //this.lastFps = timer;
-
+  update() {
     if (this.gameOver) {
       this.started = false;
       return;
     }
 
-    physics.update(fps);
+    physics.update(FPS);
 
-    for (const entity of this.entities) {
-      entity.update(fps);
+    for (const entity of this._entities) {
+      entity.update(FPS);
 
       if (entity.hp <= 0) {
         this.removeEntities([entity]);
       }
     }
 
-    // TODO: Fix the crazy spawn rate
-    if(randomInt(2000) < this.spawnInterval) {
-      this.addEntity(new Enemy(new Vector2d(617, 133), this.enemySpeed));
+    if(Math.floor(Math.random() * 1000) < this.SpawnRate) {
+      switch (Math.floor(Math.random() * 3)) {
+        case 1:
+          this.addEntity(new Enemy(new Vector2d(617, 133), this.enemySpeed, 0));
+          break;
+        case 2:
+          this.addEntity(new Enemy(new Vector2d(617, 133), this.enemySpeed, 3));
+          break;
+        case 3:
+        this.addEntity(new Enemy(new Vector2d(617, 133), this.enemySpeed, 7));
+          break;
+        default:
+          break;
+      }
+
     }
 
-    //TODO: remove coupling (dependency injection?)
-    renderer.render(fps);
-    window.requestAnimationFrame(this.updateFunc);
+    this.score++;
+    renderer.render(FPS);
+    window.requestAnimationFrame(this.updateFunction);
   }
 
   addScore(score) {
     this.highScores.push(score);
     this.highScores.sort((a, b) => b - a);
     this.highScores = this.highScores.slice(0, 10);
-
-    // TODO: Eliminate duplicated code
-    if (typeof(Storage) !== undefined) {
-      localStorage.dinoScores = JSON.stringify(this.highScores);
-    }
+    io.SaveHighScores(this.highScores);
   }
 
   setGameOver() {
     this.gameOver = true;
     this.addScore(Math.round(this.score));
-  }
-
-  //TODO: Can I convert it to full properties?
-  getEntities() {
-    return this.entities;
-  }
-
-  getFieldRect() {
-    return this.fieldRect;
-  }
-
-  getEnemies() {
-    return this.enemies;
-  }
-
-  getPlayer() {
-    return this.player;
   }
 }
 
